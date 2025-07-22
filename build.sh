791fabb7afed645ff833079137074aaae2f721d9
lumiknit <aasr4r4@gmail.com>
2025-07-22 14:36:53 +0900

    fix

--- build.sh
+++ build.sh
@@ -2,27 +2,47 @@
 
 set -e
 
-# Decompile with Apktool (decode resources + classes)
-wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.12.0.jar -O apktool.jar
-java -jar apktool.jar d brave.apk -r -o b-ex
-rm -rf b-ex/META-INF
+EXTRACTED=b-ex
 
-pushd b-ex
+if [ -d "$EXTRACTED" ]; then
+	echo "[NOTE] Directory $EXTRACTED already exists."
+else
+	# Decompile with Apktool (decode resources + classes)
+	echo "[INFO] Downloading apktool"
+	wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.12.0.jar -O apktool.jar
+
+	echo "[INFO] Decompiling Brave browser APK"
+	java -jar apktool.jar d brave.apk -r -o $EXTRACTED
+
+	echo "[INFO] Removing META-INF directory"
+	rm -rf $EXTRACTED/META-INF
+fi
+
+pushd $EXTRACTED
 
 function patch() {
 	local name="$1"
 	echo "[INFO] Patching $name"
+	# If finishes with '.rb' run with ruby
+	if [[ "$name" == *.rb ]]; then
+		ruby "$name"
+		return
+	fi
 	. "$name"
 }
 
-patch ../patches/0000-black.sh
+echo "[INFO] Patching resources"
+
+patch ../patches/0000-black.rb
 
 popd
 
 # Recompile the APK
+echo "[INFO] Recompiling APK"
 java -jar apktool.jar b b-ex -o b-patched.apk
 
 # Align and sign the APK
+echo "[INFO] Aligning the APK"
 zipalign 4 b-patched.apk b-signed.apk
 
 # Clean up
